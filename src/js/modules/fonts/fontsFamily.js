b7378b16e8b1cfd521ad6d2d1179e32455c80039
Ruper <sfc-ruper@mail.ru>
2023-10-12 10:41:48 +0300

    change events listener methods and structure fontsFamily massive

---
+++ src/js/modules/fonts/fontsFamily.js
@@ -0,0 +1,43 @@
+/*Массив доступных шрифтов*/
+export const fonts = [
+	{
+		family : "BebasNeue",
+		weight : [400]
+	},
+	{
+		family : "Montserrat",
+		weight : [100,200,300,400,500,600,700,800,900]
+	},
+	{
+		family : "NotoSans",
+		weight : [100,200,300,400,500,600,700,800,900]
+	},
+	{
+		family : "OpenSans",
+		weight : [300,400,500,600,700,800]
+	},
+	{
+		family : "Oswald",
+		weight : [200,300,400,500,600,700]
+	},
+	{
+		family : "PTSerif",
+		weight : [400,700]
+	},
+	{
+		family : "Roboto",
+		weight : [100,300,400,500,700,900]
+	},
+	{
+		family : "Raleway",
+		weight : [400]
+	},
+	{
+		family : "Rubik",
+		weight : [300,400,500,600,700,800,900]
+	},
+	{
+		family : "Ubuntu",
+		weight : [300,400,500,700]
+	},
+]
